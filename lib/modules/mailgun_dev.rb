9eef1e635a7a8660e610e411f261a7efdc4d3c59
Carlos Torres <carlosandrestorres28@hotmail.com>
2016-07-10 23:59:11 -0500

    heroku config

--- lib/modules/mailgun_dev.rb
+++ lib/modules/mailgun_dev.rb
@@ -1,39 +1,42 @@
 module MailgunDev
   class Client
-    attr_reader :private_key
+    attr_reader :private_key, :domain_name, :from
 
     def initialize
-      @private_key = "key-e9f56c927149acf55b4de412bb659efc"
+      @private_key = ENV["PRIVATE_KEY_MAILGUN"]
+      @domain_name = ENV["DOMAIN_NAME_MAILGUN"]
+      @from        = ENV["FROM_MAILGUN"]
     end
 
     def send_email(email_to, subject, text, campaign_id)
       RestClient.post "https://api:#{@private_key}"\
-      "@api.mailgun.net/v3/sandbox3ba8b9ab95324c8fa1c53581f0d9b805.mailgun.org/messages",
-      :from => "Mailgun Sandbox <postmaster@sandboxbddd8558c01a477095acb901296c3914.mailgun.org>",
-      :to => "carlos <carlosandrestorres28@gmail.com>",
+      "#{@domain_name}/messages",
+      :from => @from,
+      #:to => "carlos <carlosandrestorres28@gmail.com>",
+      :to => email_to,
       :subject => subject,
       :text => text,
       "o:campaign" => campaign_id
     end
 
     def campaigns
-      request = RestClient.get "https://api:#{@private_key}@api.mailgun.net/v3/sandbox3ba8b9ab95324c8fa1c53581f0d9b805.mailgun.org/campaigns"
+      request = RestClient.get "https://api:#{@private_key}#{@domain_name}/campaigns"
       items = JSON.parse(request)['items']
       items.map { |campaign| [campaign['name'], campaign['id']] }
     end
 
     def create_campaign(name)
-      RestClient.post "https://api:#{@private_key}@api.mailgun.net/v3/sandbox3ba8b9ab95324c8fa1c53581f0d9b805.mailgun.org/campaigns",
+      RestClient.post "https://api:#{@private_key}#{@domain_name}/campaigns",
       :name => name
     end
 
     def check_bounce(email)
-      RestClient.get("https://api:#{@private_key}@api.mailgun.net/v3/sandbox3ba8b9ab95324c8fa1c53581f0d9b805.mailgun.org/bounces"\
+      RestClient.get("https://api:#{@private_key}#{@domain_name}/bounces"\
       "/#{email}"){|response, request, result| response }
     end
 
     def get_logs(email)
-      request = RestClient.get "https://api:#{@private_key}@api.mailgun.net/v3/sandbox3ba8b9ab95324c8fa1c53581f0d9b805.mailgun.org/events",
+      request = RestClient.get "https://api:#{@private_key}#{@domain_name}/events",
       params: {
         recipient:  email
       }
